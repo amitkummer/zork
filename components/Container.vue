6e966cab1685743d2baa3a7e7c172f11b2f77388
Amit <amit.kummer@gmail.com>
2022-04-17 16:16:27 +0300

    Initial commit.

---
+++ components/Container.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<style lang="scss">
+  .container {
+    
+    &-test {
+      border: 2px solid red;      
+      width: 200px;
+      height: 200px;
+    }
+  }
+</style>
+
+<template>
+  <div class="container-test">
+    
+  </div>
+</template>
+
