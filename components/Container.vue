2feaae888f8a7685c96e9493a683ce7b49febeed
Amit <amit.kummer@gmail.com>
2022-04-17 16:52:39 +0300

    Add global SCSS file which is included in every page
    Set `assets/main.scss` as a global stylesheet that will be included in
    every page of the app.

--- components/Container.vue
+++ components/Container.vue
@@ -6,7 +6,8 @@ import { ref } from 'vue'
   .container {
     
     &-test {
-      border: 2px solid red;      
+      border: 2px solid;
+      border-color: var(--color-primary-b);
       width: 200px;
       height: 200px;
     }
